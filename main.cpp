6f50fdd44e67e2b782182cdb4509f549e914135d
siam1299 <mosiam4795@gmail.com>
2026-04-23 23:14:00 +0600

    Completed basket, score, and catch module

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <GL/glut.h>
+#include <stdio.h>
 
 // Global variables
 float basketX = 300.0f;
 float basketY = 40.0f;
 int score = 0;
+
+// Function prototypes
+void drawBasket();
+void moveBasketKeyboard(int key, int x, int y);
+void moveBasketMouse(int x, int y);
+void checkCatch(float objectX, float objectY, int points);
+void updateScore(int points);
+void drawScoreText();
+void display();
 
 void drawBasket() {
     glColor3f(1.0f, 1.0f, 0.0f);
@@ -23,7 +33,7 @@ void moveBasketKeyboard(int key, int x, int y) {
     else if (key == GLUT_KEY_RIGHT) {
         basketX += 20.0f;
     }
-
+    
     if (basketX < 30) basketX = 30;
     if (basketX > 770) basketX = 770;
 
@@ -31,35 +41,58 @@ void moveBasketKeyboard(int key, int x, int y) {
 }
 
 void moveBasketMouse(int x, int y) {
+    basketX = (float)x;
 
+    if (basketX < 30) basketX = 30;
+    if (basketX > 770) basketX = 770;
+
+    glutPostRedisplay();
 }
 
-void checkCatch() {
-
+void checkCatch(float objectX, float objectY, int points) {
+    if (objectY <= basketY + 20 &&
+        objectX >= basketX - 30 &&
+        objectX <= basketX + 30) {
+        updateScore(points);
+    }
 }
 
 void updateScore(int points) {
-
+    score += points;
 }
 
 void drawScoreText() {
+    char text[50];
+    sprintf(text, "Score: %d", score);
 
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glRasterPos2f(20.0f, 570.0f);
+
+    for (int i = 0; text[i] != '\0'; i++) {
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, text[i]);
+    }
 }
 
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
     drawBasket();
-    glFlush();
+    drawScoreText();
+
+    glutSwapBuffers();
 }
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(800, 600);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("Catch The Eggs");
 
-    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
@@ -67,6 +100,7 @@ int main(int argc, char** argv) {
 
     glutDisplayFunc(display);
     glutSpecialFunc(moveBasketKeyboard);
+    glutPassiveMotionFunc(moveBasketMouse);
 
     glutMainLoop();
     return 0;
